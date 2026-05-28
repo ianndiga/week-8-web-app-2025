e71f53ae31354eeda818f1dc37082fd67be368fe
ian ndiga <ianndiga0@gmail.com>
2026-05-28 16:55:01 +0300

    add

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -12,7 +12,7 @@ const sendTokenResponse = (user, statusCode, res) => {
 
     const options = {
         expires: new Date(
-            Date.now() + 30 * 24 * 60 * 60 * 1000 // 30 days
+            Date.now() + 30 * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
     };
